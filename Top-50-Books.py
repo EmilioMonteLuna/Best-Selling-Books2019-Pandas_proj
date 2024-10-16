8da04ae4b1c4fc2b5d07b4a9b07bce1726bd6070
Emilio Montelongo Luna <69567234+Papaemo@users.noreply.github.com>
2024-10-16 15:36:52 -0600

    Add files via upload

---
+++ Top-50-Books.py
@@ -0,0 +1,41 @@
+
+# Step 1
+import pandas as pd
+
+# Step 2
+df = pd.read_csv('bestsellers with categories.csv')
+
+# Step 3
+# Get the first 5 rows of the spreadsheet
+print(df.head())
+
+# Get the shape of the spreadsheet
+print(df.shape)
+
+# Get the column names of the spreadsheet
+print(df.columns)
+
+# Get summary statistics for each column
+print(df.describe())
+
+# Step 4
+df.drop_duplicates(inplace=True)
+
+df["Price"] = df["Price"].astype(float)
+
+df.rename(columns={"Name": "Title", "Year": "Publication Year", "User Rating": "Rating"}, inplace=True)
+
+df["Price"] = df["Price"].astype(float)
+
+# Step 5
+author_counts = df['Author'].value_counts()
+print(author_counts)
+
+avg_rating_by_genre = df.groupby("Genre")["Rating"].mean()
+print(avg_rating_by_genre)
+
+# Step 6
+author_counts.head(10).to_csv("top_authors.csv")
+# Export average rating by genre to a CSV file
+avg_rating_by_genre.to_csv("avg_rating_by_genre.csv")
+
